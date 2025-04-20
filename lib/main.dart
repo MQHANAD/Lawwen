f87a0707d7cd735efc97c21a44ea7769648af03e
Rashed <r45hed3@gmail.com>
2025-04-21 00:37:50 +0300

    Keep user logged in + fixed android app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'Home.dart';
 import 'email_screen.dart';
 import 'firebase_config.dart';
 
@@ -10,23 +13,44 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
 
-  await Firebase.initializeApp(
-    options: getFirebaseOptions(),
-  );
-
+  // Initialize Firebase conditionally based on platform
+  if (Firebase.apps.isEmpty) {
+    if (kIsWeb) {
+      await Firebase.initializeApp(
+        options: getFirebaseOptions(),
+      );
+    } else {
+      await Firebase.initializeApp(
+        name: 'lawwenApp',
+        options: getFirebaseOptions(),
+      );
+    }
+  }
   runApp(const MyApp());
 }
 
 /// Root widget.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lawwen',
-      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       debugShowCheckedModeBanner: false,
-      home: const OnboardingScreen(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasData) {
+            return const HomeScreen(); //  User is logged in
+          } else {
+            return const OnboardingScreen(); //  User is NOT logged in
+          }
+        },
+      ),
     );
   }
 }
